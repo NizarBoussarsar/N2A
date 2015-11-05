09233e178898f39a4cec15ca60913bae2789df41
NizarBoussarsar <nizar.boussarsar@esprit.tn>
2015-11-05 10:56:31 +0100

    Added onLonClickLisner on rowView to select/unselect an image Added a button to compare th two lists Trying to add instructions for first use

--- app/src/main/java/ti/iam/ifi/polytech/unice/myapplication/CustomAdapter.java
+++ app/src/main/java/ti/iam/ifi/polytech/unice/myapplication/CustomAdapter.java
@@ -1,6 +1,7 @@
 package ti.iam.ifi.polytech.unice.myapplication;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,18 +9,26 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomAdapter extends ArrayAdapter<Integer> {
 
     private static LayoutInflater inflater = null;
-    Activity context;
-    Integer[] imgs;
-    Integer[] ids;
+    public List<Integer> selectedImages;
+    private Activity context;
+    private Integer[] imgs;
+    private Integer[] ids;
+    private boolean[] states;
 
     public CustomAdapter(Activity context, Integer[] ids, Integer[] imgs) {
         super(context, R.layout.image_list_view, ids);
         this.context = context;
         this.ids = ids;
         this.imgs = imgs;
+
+        states = new boolean[ids.length];
+        selectedImages = new ArrayList<>();
     }
 
     @Override
@@ -29,15 +38,38 @@ public class CustomAdapter extends ArrayAdapter<Integer> {
 
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
-        View rowView = inflater.inflate(R.layout.image_list_view, null, true);
+        final View rowView = inflater.inflate(R.layout.image_list_view, null, true);
+        if (states[position]) rowView.setBackgroundColor(Color.GREEN);
+
 
         ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView1);
         TextView textView = (TextView) rowView.findViewById(R.id.textView);
 
         textView.setText("Number " + ids[position]);
         imageView.setImageResource(imgs[position]);
+
+        rowView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                if (!selectedImages.contains(ids[position])) {
+                    selectedImages.add(ids[position]);
+                    rowView.setBackgroundColor(Color.GREEN);
+                    states[position] = true;
+                    rowView.invalidate();
+                } else {
+                    selectedImages.remove(ids[position]);
+                    rowView.setBackgroundColor(Color.RED);
+                    states[position] = false;
+                    rowView.invalidate();
+                }
+
+                return false;
+            }
+        });
+
         return rowView;
     }
 }
--- app/src/main/java/ti/iam/ifi/polytech/unice/myapplication/MainActivity.java
+++ app/src/main/java/ti/iam/ifi/polytech/unice/myapplication/MainActivity.java
@@ -7,8 +7,14 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
@@ -35,6 +41,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     private ListView listView;
     private TextView textView;
+    private Button button;
 
     private SensorManager sensorManager;
     private Sensor sensor;
@@ -42,26 +49,73 @@ public class MainActivity extends Activity implements SensorEventListener {
     private int initScrollPos = 0;
     private float initialY, yThreshold = 2;
     private boolean isfirstRun = true, isReternedToOriginalPosition = true;
+    private CustomAdapter adapter;
+
+    private List<Integer> correctImages;
+    View.OnClickListener buttonHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (adapter.selectedImages.retainAll(correctImages)) {
+                Toast.makeText(getApplicationContext(), "TRUE", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "FALSE", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        if (isFirstTime()) {
+            ShowcaseView showcaseView = new ShowcaseView.Builder(this)
+                    .setTarget(new ActionViewTarget(this, ActionViewTarget.Type.HOME))
+                    .setContentTitle("Instructions")
+                    .setContentText("This is highlighting the Home button")
+                    .hideOnTouchOutside()
+                    .build();
+        }
+        */
+
         imagesIds = new Integer[images.length];
+
+        correctImages = new ArrayList<>();
+        correctImages.add(images[0]);
+        correctImages.add(images[images.length - 1]);
 
         for (int i = 0; i < images.length; i++) {
             imagesIds[i] = i;
         }
 
+        adapter = new CustomAdapter(this, imagesIds, images);
+
         listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(new CustomAdapter(this, imagesIds, images));
+        listView.setAdapter(adapter);
+        listView.invalidate();
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(buttonHandler);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    /*
+    private boolean isFirstTime() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        boolean ranBefore = preferences.getBoolean("RanBefore", false);
+        if (!ranBefore) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("RanBefore", true);
+            editor.commit();
+        }
+        return !ranBefore;
+    }
+    */
+    
     @Override
     protected void onPause() {
         super.onPause();
